fa4eb5616cbf2f4249e74498190c1b979c70ea3d
Totendra <hiremathtotendra714@gmail.com>
2024-10-30 06:50:06 +0530

    add all the 3 files to my cart.com

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example for handling Add to Cart functionality
+document.querySelectorAll('.add-to-cart').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Product added to cart!');
+        // Add more logic for cart functionality here
+    });
+});
+
+// You can implement login and signup functionality as needed
